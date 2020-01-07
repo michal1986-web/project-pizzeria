5795e98862e588517d66246ffd029c2a0da6450c
Michał Grzegorczyk <michalgrzegorczyk4@gmail.com>
2020-01-07 11:48:59 +0100

    Add Booking class

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,68 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import {Product} from './components/Product.js';
 import {Cart} from './components/Cart.js';
+import {Booking} from './components/Booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* TODO: get page id from href */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* TODO: activate page */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for (let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
+  initBooking: function(){
+    const thisApp = this;
+    
+    const bookingElem = document.querySelector(select.containerOf.booking);
+
+    thisApp.booking = new Booking(bookingElem);
+  },
+
   initMenu: function(){
     const thisApp = this;
 
@@ -52,8 +112,10 @@ const app = {
     //console.log('classNames:', classNames);
     //console.log('settings:', settings);
     //console.log('templates:', templates);
-    thisApp.initCart();
+    thisApp.initPages();
     thisApp.initData();
+    thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
